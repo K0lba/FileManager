317c7b2cd864e7f4fa264169aef3ad3eba4eb0ce
K0lba <a.rudskoj@icloud.com>
2022-04-14 16:23:03 +0300

    a

---
+++ FileManager1/BookSite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileManager1
+{
+    public partial class BookSite : Form
+    {
+        public BookSite()
+        {
+            InitializeComponent();
+        }
+    }
+}
