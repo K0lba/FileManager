f3adcef5442ee18e0c8c38034ac4563009951e6b
K0lba <a.rudskoj@icloud.com>
2022-04-14 17:05:37 +0300

    ц

--- FileManager1/BookSite.Designer.cs
+++ FileManager1/BookSite.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 63);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(776, 364);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(694, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(632, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(46, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(578, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Count";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Python",
+            "C#",
+            "C++",
+            "JavaScript",
+            "Kotlin",
+            "PHP",
+            "SQL",
+            "ASM",
+            "Java"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(560, 28);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // BookSite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "BookSite";
             this.Text = "BookSite";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private ComboBox comboBox1;
     }
 }
--- FileManager1/BookSite.cs
+++ FileManager1/BookSite.cs
@@ -1,20 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Net;
+using System.Text.RegularExpressions;
 
 namespace FileManager1
 {
     public partial class BookSite : Form
     {
+        string progLang;
+        int pages;
+
         public BookSite()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progLang = comboBox1.Text;
+            pages = Convert.ToInt32(textBox1.Text);
+
+            string g = "Python docodcko,Pythone ek";
+            Regex regex = new Regex(@"Python(\w*)");
+            MatchCollection matches = regex.Matches(g);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    listBox1.Items.Add(match.Value);
+            }
+            WebClient client = new WebClient();
+
+            using (Stream stream = client.OpenRead("https://docs.microsoft.com/ru-ru/dotnet/api/system.net.webclient.openread?view=netframework-4.8"))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        listBox1.Items.Add(line.Normalize());
+                    }
+                }
+            }
+
+        }
+
     }
 }
